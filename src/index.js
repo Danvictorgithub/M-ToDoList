05a9478a2607075985b003532c8d1c4b74b14422
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2022-07-20 13:14:40 +0800

    Fixed showAddProject bad practice:used CSS show/hide instead of JS DOM Creation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './style.css';
 // JS DOMs
 const addProjectbtn = document.querySelector('.btn');
 const sideBar = document.querySelector('.side-bar');
+const projectTaskNav = document.querySelector('.project-task');
+const createProjectform = document.querySelector('.create-project');
 class task{
     constructor(name,dueDate,index) {
         this.name = name;
@@ -13,39 +15,54 @@ class task{
 let index = [];
 
 addProjectbtn.addEventListener('click',showAddProjectForm);
-function showAddProjectForm() {
-    sideBar.removeChild(sideBar.lastElementChild);
-    const projectForm = document.createElement('div');
-    projectForm.classList.add('create-project');
-        const projectHeader = document.createElement('div');
-        projectForm.appendChild(projectHeader);
-            const labelProjectName = document.createElement('label');
-            labelProjectName.setAttribute('for','project-name');
-            labelProjectName.textContent = 'Project Name';
-            projectHeader.appendChild(labelProjectName);
+// function showAddProjectForm() {
+//     hideAddProjectbtn();
+//     const projectForm = document.createElement('div');
+//     projectForm.classList.add('create-project');
+//         const projectHeader = document.createElement('div');
+//         projectForm.appendChild(projectHeader);
+//             const labelProjectName = document.createElement('label');
+//             labelProjectName.setAttribute('for','project-name');
+//             labelProjectName.textContent = 'Project Name';
+//             projectHeader.appendChild(labelProjectName);
 
-            const inputProjectName = document.createElement('input');
-            inputProjectName.setAttribute('id','project-name');
-            inputProjectName.setAttribute('type','text');
-            projectHeader.appendChild(inputProjectName);
+//             const inputProjectName = document.createElement('input');
+//             inputProjectName.setAttribute('id','project-name');
+//             inputProjectName.setAttribute('type','text');
+//             projectHeader.appendChild(inputProjectName);
         
-        const projectButtons = document.createElement('div');
-        projectButtons.classList.add('create-project-btn');
-        projectForm.appendChild(projectButtons);
-            const addButton = document.createElement('button');
-            addButton.setAttribute('type','button');
-            addButton.classList.add('add-project');
-            addButton.textContent = 'Add';
-            projectButtons.appendChild(addButton);
+//         const projectButtons = document.createElement('div');
+//         projectButtons.classList.add('create-project-btn');
+//         projectForm.appendChild(projectButtons);
+//             const addButton = document.createElement('button');
+//             addButton.setAttribute('type','button');
+//             addButton.classList.add('add-project');
+//             addButton.textContent = 'Add';
+//             projectButtons.appendChild(addButton);
 
-            const cancelButton = document.createElement('button');
-            cancelButton.setAttribute('type','button');
-            cancelButton.classList.add('cancel-project');
-            cancelButton.textContent = 'Cancel';
-            projectButtons.appendChild(cancelButton);
-    
-    sideBar.appendChild(projectForm);
+//             const cancelButton = document.createElement('button');
+//             cancelButton.setAttribute('type','button');
+//             cancelButton.classList.add('cancel-project');
+//             cancelButton.textContent = 'Cancel';
+//             projectButtons.appendChild(cancelButton);
+//     sideBar.appendChild(projectForm);
+// }
+function showAddProjectForm() {
+    hideAddProjectbtn();
+    createProjectform.style.display = 'flow';
 }
+function showAddProjectbtn() {
+    projectTaskNav.style.display = 'flow';
+    createProjectform.style.display = 'none';
+}
+function hideAddProjectbtn() {
+    projectTaskNav.style.display = 'none';
+}
+(function projectDOM() {
+    const addButton = document.querySelector('.add-project');
+    const cancelButton = document.querySelector('.cancel-project'); 
+    cancelButton.addEventListener('click', showAddProjectbtn);
+})();
 
 
 
